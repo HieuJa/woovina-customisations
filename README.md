c8bb4b357f4f2da5dd3c8a9c19910deb6f600d47
HieuJa <mr_hiennc@yahoo.com>
2019-01-30 09:43:11 +0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# woovina-customisations
-A handy little plugin to contain your theme customisation snippets.
---
+++ custom/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Functions.php
+ *
+ * @package  WooVina_Theme_Customisations
+ * @author   WooVina Theme
+ * @since    1.0
+ */
+
+if(! defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * functions.php
+ * Add PHP snippets here
+ */
